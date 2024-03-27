2e557cbb1f1588f3babc129dbb6767bb6d1dfe74
Dyana Butler <dyanajbutler98@gmail.com>
2024-03-27 13:05:08 -0400

    new

--- src/app/page.js
+++ src/app/page.js
@@ -5,25 +5,24 @@ import Hero from "./components/Hero";
 import Video from "./components/Video";
 import Scroll from "./components/Scroll";
 import FaqComponent from "./components/Faq";
-import Background from "./components/Background";
+import HeroCards from "./components/HeroCards";
+import "./globals.css";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8">
- 
- 
-  <div className="background-image" style={{ backgroundImage: `url(/bg-1.svg)`, backgroundSize: "100% 100%", backgroundPosition: "top center" }}>
-    <Navbar />
-    <Hero />
-    <EmailForm />
-    <Scroll />
-    </div>
-    <Video />
-  
-  <FaqComponent />
-
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="background-image w-full" style={{ backgroundImage: `url(/bg-1.svg)`, backgroundSize: "cover", backgroundPosition: "top center" }}>
+        <Navbar />
+        <Hero />
+        <EmailForm />
+        <Scroll />
+      </div>
       
-      
+      {/* HeroCards section with Tailwind styling for spacing and layout */}
+     
 
-      </main>
+      <Video />
+      <FaqComponent />
+    </main>
   );
 }
