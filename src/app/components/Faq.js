75b691a55ccfa1b3750ae9251c42455de33f831a
Dyana Butler <dyanajbutler98@gmail.com>
2024-07-24 15:53:30 -0400

    added impacts

--- src/app/components/Faq.js
+++ src/app/components/Faq.js
@@ -38,15 +38,34 @@ export default function Component() {
         <Collapsible>
           <CollapsibleTrigger asChild>
             <div className="flex items-center justify-between px-4 py-2 rounded-lg cursor-pointer bg-black dark:bg-gray-800 hover:bg-orange-600 dark:hover:bg-gray-700 transition-colors">
-              <h2 className="text-lg md:text-xl lg:text-2xl font-semibold">How are you different from other NFTS?</h2>
+              <h2 className="text-lg md:text-xl lg:text-2xl font-semibold">What are impacts?</h2>
               <ChevronDownIcon className="h-5 w-5" />
             </div>
           </CollapsibleTrigger>
+          
           <CollapsibleContent className="px-4 py-2 rounded-lg bg-gray-100 dark:bg-gray-800">
             <p className="text-sm md:text-base lg:text-lg text-gray-600 dark:text-gray-400">
             
 
-WRLD CHNGRS provides a unique digital collectible experience with our hero builder, allowing you to customize your character&apos;s poses, body types, skin tones, and accessories. Once all 4,444 heroes are sold, our secret bonus feature will be unveiled.
+            Impacts in WRLD CHNGRS refer to the positive actions and achievements you accomplish in connections here with WRLD CHNGRS. These could align with your real-world actions on issues like environmental conservation or social justice and so many more. Each impact not only progresses your impact level in our universe but also mirrors positive changes in the real world, emphasizing our commitment to making a difference. 
+
+
+            </p>
+          </CollapsibleContent>
+        </Collapsible>
+        <Collapsible>
+          <CollapsibleTrigger asChild>
+            <div className="flex items-center justify-between px-4 py-2 rounded-lg cursor-pointer bg-black dark:bg-gray-800 hover:bg-orange-600 dark:hover:bg-gray-700 transition-colors">
+              <h2 className="text-lg md:text-xl lg:text-2xl font-semibold">How are you different from other NFTS?</h2>
+              <ChevronDownIcon className="h-5 w-5" />
+            </div>
+          </CollapsibleTrigger>
+          
+          <CollapsibleContent className="px-4 py-2 rounded-lg bg-gray-100 dark:bg-gray-800">
+            <p className="text-sm md:text-base lg:text-lg text-gray-600 dark:text-gray-400">
+            
+
+            WRLD CHNGRS provides a unique digital collectible experience with our hero builder, allowing you to customize your character&apos;s poses, body types, skin tones, and accessories. Once all 4,444 heroes are sold, our secret bonus feature will be unveiled.
 
 
             </p>
